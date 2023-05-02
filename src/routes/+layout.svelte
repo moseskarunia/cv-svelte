e8fba98cf6e24d2e792b310d6377d3da1d76cbb6
Moses Karunia <moseskarunia@gmail.com>
2023-05-02 08:04:56 +0000

    add header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,12 @@
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500&family=Peralta&display=swap"
+    rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src="https://kit.fontawesome.com/d0c488930b.js" crossorigin="anonymous"></script>
+</svelte:head>
+
 <div class="parent">
 	<div class="sidebar" />
 	<div class="content">
