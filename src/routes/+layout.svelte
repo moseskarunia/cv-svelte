b7bc9c29cac0d63071f723ec3ece9b0f64549a25
Moses Karunia <moseskarunia@gmail.com>
2023-05-02 07:57:28 +0000

    sidebar exercise

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,32 @@
+<div class="parent">
+	<div class="sidebar" />
+	<div class="content">
+		<slot />
+	</div>
+</div>
+
+<style>
+	:global(*) {
+		margin: 0;
+		padding: 0;
+	}
+	:global(html, body) {
+		width: 100%;
+		height: 100%;
+	}
+	.parent {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		flex-direction: row;
+	}
+	.sidebar {
+		background-color: blue;
+		width: 200px;
+		height: 100%;
+	}
+	.content {
+		display: flex;
+		flex-direction: column;
+	}
+</style>
