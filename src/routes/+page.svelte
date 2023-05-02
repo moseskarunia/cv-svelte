438d6016504b6412a981176fd95eeef4aeb5f907
Moses Karunia <moseskarunia@gmail.com>
2023-05-02 08:22:12 +0000

    add cv hml

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,107 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="readable-area">
+	<a class="header" href="./photo.html">
+		<div class="header-text">
+			<h1>Squarepants, Spongebob</h1>
+			<p>Head Chef</p>
+		</div>
+		<img
+			alt="spongebob avatar"
+			class="avatar"
+			src="https://play-lh.googleusercontent.com/9XA4q4TKYtF2l5oN10M8oumPauR9g5lFOAJXjczrd3Gmq9cwQ8QOQJ-2KZv84s1oTA"
+		/>
+	</a>
 
-<a href="/testRoute">Go to test route</a>
-<a href="/secondRoute">Go to second route</a>
+	<div class="content">
+		<div class="content-column">
+			<div class="section-title">
+				<i class="fa fa-solid fa-user" />
+				Profile
+			</div>
+			<br />
+			<p>
+				Hello, my name is Spongebob Squarepants and I'm passionate about creating the best possible
+				food. In my spare time, I like to catch jellyfishes with my trusty net.
+			</p>
+			<br />
+			<div class="section-title">
+				<i class="fa fa-solid fa-book" />
+				Education
+			</div>
+			<br />
+			<ul>
+				<li>
+					2011-2014
+					<br />
+					&emsp; Bikini Bottom High School
+				</li>
+				<li>
+					2014-2018
+					<br />
+					&emsp; New Kelp City Culinary College
+				</li>
+			</ul>
+		</div>
+		<div class="content-column">
+			<div class="section-title">
+				<i class="fa fa-solid fa-briefcase" />
+				Work History
+			</div>
+			<br />
+			<ul>
+				<li>
+					Head Cook (2022 - ongoing)
+					<br />
+					&emsp; Krusty Krab, Inc.
+				</li>
+				<li>
+					Marketing Associate (2020 - 2022)
+					<br />
+					&emsp; Chum Bucket Ltd.
+				</li>
+				<li>
+					Jellyfish Catcher (2018 - 2020)
+					<br />
+					&emsp; Freelance
+				</li>
+			</ul>
+			<br />
+			<div class="section-title">
+				<i class="fa fa-solid fa-wand-sparkles" />
+				Skills
+			</div>
+			<br />
+			<ul>
+				<li>
+					<span>Jellyfish Hunting</span>
+					<div class="gauge">
+						<div class="bullet filled-bullet" />
+						<div class="bullet filled-bullet" />
+						<div class="bullet filled-bullet" />
+						<div class="bullet filled-bullet" />
+						<div class="bullet filled-bullet" />
+					</div>
+				</li>
+				<li>
+					<span>English</span>
+					<div class="gauge">
+						<div class="bullet filled-bullet" />
+						<div class="bullet filled-bullet" />
+						<div class="bullet filled-bullet" />
+						<div class="bullet filled-bullet" />
+						<div class="bullet" />
+					</div>
+				</li>
+				<li>
+					<span>Whale Singing</span>
+					<div class="gauge">
+						<div class="bullet filled-bullet" />
+						<div class="bullet filled-bullet" />
+						<div class="bullet filled-bullet" />
+						<div class="bullet" />
+						<div class="bullet" />
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
