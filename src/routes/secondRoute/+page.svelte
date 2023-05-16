5964cfa40d7532c69e644044124ce18105bf3d1b
Moses Karunia <moseskarunia@gmail.com>
2023-05-16 07:32:25 +0000

    page for displaying images

--- src/routes/secondRoute/+page.svelte
+++ src/routes/secondRoute/+page.svelte
@@ -1,3 +1,22 @@
+<script lang="ts">
+	import Strawberry from '$lib/assets/strawberry.jpeg';
+</script>
+
 <h1>Welcome to SecondRoute</h1>
 
+<!--Image from link-->
+<img
+	src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Garden_strawberry_%28Fragaria_%C3%97_ananassa%29_single2.jpg/800px-Garden_strawberry_%28Fragaria_%C3%97_ananassa%29_single2.jpg"
+	alt="Strawberry"
+/>
+
+<!--Image from assets-->
+<img src={Strawberry} alt="Strawberry" />
+
 <a href="/">Go to main page</a>
+
+<style>
+	img {
+		display: none;
+	}
+</style>
